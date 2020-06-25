4df8a493b49d253371e49ef83279f44ec7c6bfc6
Pedro <pcunhavieira@gmail.com>
2020-06-25 01:40:44 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoMVCB32/BLL/tblClienteBLL.cs
@@ -0,0 +1,29 @@
+﻿using ProjetoMVCB32.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoMVCB32.BLL
+{
+    class tblClienteBLL
+    {
+        private DALMysql DaoBanco = new DALMysql();
+
+        public Boolean Autenticar(string cpf, string nomecliente, string nomemae)
+        {
+            string consulta = string.Format($@"select * from tbl_clienteb32 where cpf_cliente = '{cpf}' and nome_cliente = '{nomecliente}' and nome_mae = '{nomemae}' ;");
+            DataTable dt = DaoBanco.executarConsulta(consulta);
+            if (dt.Rows.Count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
